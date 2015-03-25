de3d306b686a24b92a486ef6a5ad5ae1aad32064
jacobford8 <jford@fishsixrc.com>
2015-03-25 16:22:16 -0700

    Adding Movie App

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,14 @@
+class MoviesController < ApplicationController
+  def index
+  	@movies = Movie.all
+  end	
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/movies_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class MoviesControllerTest < ActionController::TestCase
+  test "should get view" do
+    get :view
+    assert_response :success
+  end
+
+  test "should get add" do
+    get :add
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
+  test "should get search" do
+    get :search
+    assert_response :success
+  end
+
+end
